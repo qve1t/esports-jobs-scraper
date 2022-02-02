7742f471bfc9d09d417be4e20f1f043885dc2926
qve1t <warpas.piotr@gmail.com>
2022-02-02 11:50:13 +0100

    move some component from compoents folder directly to pages

--- client/src/pages/NoPage.tsx
+++ client/src/pages/NoPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import NotFound from "../components/404";
 import useGaTracker from "../hooks/useGaTracker";
 
 const NoPage = () => {
@@ -10,9 +9,10 @@ const NoPage = () => {
   useGaTracker();
 
   return (
-    <>
-      <NotFound />
-    </>
+    <div className="text-5xl dark:text-white text-gray-200 ">
+      <span className="text-pink-700 font-bold text-9xl">404</span> page not
+      found!
+    </div>
   );
 };
 
--- client/src/pages/OfferPage.tsx
+++ client/src/pages/OfferPage.tsx
@@ -1,13 +1,72 @@
-import DetailedOffer from "../components/DetailedOffer";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getOfferDetails } from "../api/offers";
+import { JobOffer } from "../interfaces/JobOffer.interface";
+import { PageLoadingStateInterface } from "../interfaces/pageState.interface";
+import { LinkButton } from "../components/Buttons";
+import CompanyLink from "../components/CompanyLink";
+import ErrorComponent from "../components/ErrorComponent";
+import LoadingComponent from "../components/LoadingComponent";
 import useGaTracker from "../hooks/useGaTracker";
 
 const OfferPage = () => {
   useGaTracker();
 
+  const [data, setData] = useState<JobOffer | null>(null);
+  const [pageLoadingState, setPageLoadingState] =
+    useState<PageLoadingStateInterface>({
+      error: null,
+      loading: true,
+    });
+  const { id } = useParams();
+
+  useEffect(() => {
+    document.title =
+      data?.name || "esports-GO: Start your esport journey today";
+  }, [data?.name]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await getOfferDetails(id || "");
+
+      if (fetchedData.response) {
+        setPageLoadingState({ loading: false, error: null });
+        setData(fetchedData.response);
+      } else {
+        setPageLoadingState({ loading: false, error: fetchedData.error });
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  if (pageLoadingState.loading) {
+    return <LoadingComponent />;
+  }
+
+  if (pageLoadingState.error) {
+    return <ErrorComponent error={pageLoadingState.error} />;
+  }
+
   return (
-    <>
-      <DetailedOffer />
-    </>
+    <div className=" dark:text-opacity-80">
+      <div className="flex justify-between">
+        <div>
+          <p>
+            Organization: <CompanyLink company={data?.company || ""} />
+          </p>
+          <p>
+            Location:<span className="font-bold"> {data?.location}</span>
+          </p>
+        </div>
+        <LinkButton url={data?.url || ""} text="Full offer" />
+      </div>
+      <h1 className="text-5xl text-center my-8">{data?.name}</h1>
+      <div className="whitespace-pre-wrap">{data?.description}</div>
+      <div className="flex justify-start mt-8">
+        <LinkButton url={data?.url || ""} text="Full offer" />
+      </div>
+    </div>
   );
 };
 
